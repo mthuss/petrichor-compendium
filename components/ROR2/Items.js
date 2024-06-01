17f9ffad711566aa0462c45b88ec4c70be3df49e
Matheus <leirommatheus@protonmail.com>
2024-06-01 17:16:50 -0300

    started importing item data into the app

--- components/ROR2/Items.js
+++ components/ROR2/Items.js
@@ -1,8 +1,24 @@
 import { View, ImageBackground, Text, Image, FlatList } from 'react-native'
 import Styles from '../Styles'
+import { useState } from 'react'
 
 export default props => {
+    const [showCommon, setShowCommon] = useState(true)
+    const [showUncommon, setShowUncommon] = useState(true)
+    const [showLegendary, setShowLegendary] = useState(true)
+    const [showVoid, setShowVoid] = useState(true)
+    const [showLunar, setShowLunar] = useState(true)
+    const [showBoss, setShowBoss] = useState(true)
 
+    const data_dir = "../../data/ROR2/items/"
+    const items_list = {
+        "common": require(data_dir+'common_items.json'),
+        "uncommon": require(data_dir+'uncommon_items.json'),
+        "legendary": require(data_dir+'legendary_items.json'),
+        "void": require(data_dir+'void_items.json'),
+        "lunar": require(data_dir+'lunar_items.json'),
+        "boss": require(data_dir+'boss_items.json'),
+    }
     return(
         <View style={Styles.container}>
             <ImageBackground source={require('../../assets/main_bg.png')} resizeMode="cover" style={Styles.imageBackground}>
@@ -13,7 +29,7 @@ export default props => {
                     </View>
                     <View style={{flex: 1, backgroundColor: "#1a1b20", borderStyle: "solid", borderWidth: 10, borderColor:"#2c2e3a", borderRadius: 5}}>
                         <View style={{flex: 1, marginTop: 54}}>
-                            <FlatList></FlatList>
+                            <Text style={{color: "#fff"}}>{items_list["boss"][0]["itemName"]}</Text>
                         </View>
                     </View>
                 </View>
